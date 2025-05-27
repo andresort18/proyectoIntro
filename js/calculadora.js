e8b2c6745fa7d7f75812edede71698455a0ff8ff
andresort18 <andresortguerra15@gmail.com>
2025-05-26 20:29:55 -0600

    Subiendo mis cambios de la pagina web finalizada

---
+++ js/calculadora.js
@@ -0,0 +1,38 @@
+function sumar(){
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+
+    var resultado = num1 + num2;
+
+    document.getElementById("resultado").innerHTML = "La suma de los dos numeros es: " + resultado;
+}
+
+function restar(){
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+
+    var resultado = num1 - num2;
+
+    document.getElementById("resultado").innerHTML = "La suma de los dos numeros es: " + resultado;
+
+}
+
+function dividir(){
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+
+    var resultado = num1 / num2;
+
+    document.getElementById("resultado").innerHTML = "La suma de los dos numeros es: " + resultado;
+
+}
+
+function multiplicar(){
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+
+    var resultado = num1 * num2;
+
+    document.getElementById("resultado").innerHTML = "La suma de los dos numeros es: " + resultado;
+
+}
